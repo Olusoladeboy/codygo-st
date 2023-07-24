1ed93ef1b0c2f7921f7701efdea3ecef3c38dbda
olusoladeboy <goldblumie1@gmail.com>
2023-07-24 15:54:40 +0100

    Updated CDK and attempted deployment

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as s3 from "aws-cdk-lib/aws-s3";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
@@ -12,5 +15,29 @@ export class CdkStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'CdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const bucket = new s3.Bucket(this, "Store");
+
+    const handler = new lambda.Function(this, "Handler", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset("resources"),
+      handler: "index.main",
+      environment: {
+        BUCKET: bucket.bucketName
+      }
+    });
+
+    bucket.grantReadWrite(handler);
+
+    const api = new apigateway.RestApi(this, "codygo-api", {
+      restApiName: "Codygo Lambda Test",
+      description: "This service serves as a codygo lambda test."
+    });
+
+    const getIntegration = new apigateway.LambdaIntegration(handler, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    });
+
+    api.root.addMethod("GET", getIntegration); // GET /
   }
 }
