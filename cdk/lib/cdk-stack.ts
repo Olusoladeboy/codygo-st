100b1692d5708ffe5f89d9966d7f7b5be9143dd6
olusoladeboy <goldblumie1@gmail.com>
2023-07-24 18:56:33 +0100

    Updated Lambda Func

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambdanodejs from "aws-cdk-lib/aws-lambda-nodejs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -16,28 +17,35 @@ export class CdkStack extends cdk.Stack {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
 
-    const bucket = new s3.Bucket(this, "Store");
+    const bucket = new s3.Bucket(this, "WidgetStore");
 
-    const handler = new lambda.Function(this, "Handler", {
+    const handler = new lambda.Function(this, "WidgetHandler", {
       runtime: lambda.Runtime.NODEJS_18_X,
       code: lambda.Code.fromAsset("resources"),
-      handler: "index.main",
+      handler: "widgets.main",
       environment: {
         BUCKET: bucket.bucketName
       }
     });
-
     bucket.grantReadWrite(handler);
 
-    const api = new apigateway.RestApi(this, "codygo-api", {
-      restApiName: "Codygo Lambda Test",
-      description: "This service serves as a codygo lambda test."
+    const api = new apigateway.RestApi(this, "widgets-api", {
+      restApiName: "Widget Service",
+      description: "This service serves widgets."
     });
 
-    const getIntegration = new apigateway.LambdaIntegration(handler, {
+    const getWidgetsIntegration = new apigateway.LambdaIntegration(handler, {
       requestTemplates: { "application/json": '{ "statusCode": "200" }' }
     });
 
-    api.root.addMethod("GET", getIntegration); // GET /
+    api.root.addMethod("GET", getWidgetsIntegration);
+
+    const widget = api.root.addResource("{id}");
+
+    const widgetIntegration = new apigateway.LambdaIntegration(handler);
+
+    widget.addMethod("POST", widgetIntegration);   // POST /{id}
+    widget.addMethod("GET", widgetIntegration);    // GET /{id}
+    widget.addMethod("DELETE", widgetIntegration); // DELETE /{id
   }
 }
